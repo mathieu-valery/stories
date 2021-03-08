7d3458adbf2abd4b9676a7e51e5b6887ff4ca97b
Mathieu Valery <mathieuromainvalery@gmail.com>
2021-03-08 18:25:14 +0100

    create models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
+  has_many :followers, through: :received_follows, source: :follower
+  has_many :comments
+  has_many :likes
 end
