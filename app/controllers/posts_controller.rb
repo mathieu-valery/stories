7d9ef6d7fe258822c2dc7a5eaf764973abee43f9
Mathieu Valery <mathieuromainvalery@gmail.com>
2021-03-09 01:19:16 +0100

    add photo to registration form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.user = current_user
+
+        if @post.save
+            redirect_to posts_path
+        else
+            flash[:alert] = "Something went wrong."
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:video_url, :caption)
+      end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts("Destroy previous instances...")
+Like.destroy_all
+Comment.destroy_all
+Follow.destroy_all
+Post.destroy_all
+User.destroy_all
+
+puts("Done")
+
+puts("Create Users...")
+User.create(username: "Bear Grylls", email:"beargrylls@official.com", password:"helloworld")
+User.create(username: "Beyonce", email:"beyonce@official.com", password:"helloworld")
+User.create(username: "Ellon Musk", email:"ellonmusk@official.com", password:"helloworld")
+puts("Done")
+
+puts("Create Posts...")
+p = Post.new(user_id: User.first.id, video_url: "https://res.cloudinary.com/dg4hemebf/video/upload/v1615240176/oss-117-jaime-me-beurrer-la-biscotte_x0jutd.mp4", caption: "Rick Rolled")
+p.save!
+puts("Done")
+
+puts("Create Likes...")
+l = Like.create(user_id: User.first.id, post_id: Post.first.id, is_liked: true)
+l.save!
+puts("Done")
+
+puts("Create Comments...")
+c = Comment.create(user_id: User.first.id, post_id: Post.first.id, text: "Lol you got me")
+c.save!
+puts("Done")
+
+puts("Create Follows...")
+f = Follow.create(follower_id: User.first.id, followed_user_id: User.second.id)
+f.save!
+puts("Done")
